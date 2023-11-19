1f7ea4193c98b3ace7226c1dc0b1bf8e3176ff68
Lucas Souza <89395605+LotinLSV@users.noreply.github.com>
2023-11-19 17:15:49 -0300

    Update sistemaBancario.py

--- sistemaBancario.py
+++ sistemaBancario.py
@@ -14,6 +14,7 @@ operacoes = """
 
 valor_conta = 0.0
 extrato_deposito = ''
+num_saques = 0
 while True:
     escolha = input(operacoes)
     if escolha == '1':
@@ -28,16 +29,19 @@ while True:
         print("Valor de deposito precisa ser maior que 0.01")  
 
     elif escolha == '2':
-        valor_saque = float(input("Entre com o valor que deseja sacar: "))
-        if valor_saque >= 0.01 and valor_saque <= 500 and valor_saque <= valor_conta:
-            saque_moeda = "R$ {:,.2f}".format(valor_saque)
-            extrato_deposito += "Saque realizado de: "
-            extrato_deposito += f"{saque_moeda} \n"
-            print(f"Saque de {saque_moeda} realizado com sucesso.")
-            valor_conta -= valor_saque
-        else:
-           print("Valor de saque inválido.")
-
+        while num_saques < 3:
+            valor_saque = float(input("Entre com o valor que deseja sacar: "))
+            if valor_saque >= 0.01 and valor_saque <= 500 and valor_saque <= valor_conta:
+                saque_moeda = "R$ {:,.2f}".format(valor_saque)
+                extrato_deposito += "Saque realizado de: "
+                extrato_deposito += f"{saque_moeda} \n"
+                print(f"Saque de {saque_moeda} realizado com sucesso.")
+                valor_conta -= valor_saque
+                num_saques += 1
+            else:
+                print("Saque inválido.")
+        
+        print("Seu número de saques diários foi atingido tente outro dia.")
     elif escolha == '3':
        print(f"{extrato_deposito}")
        saldo_conta = "R$ {:,.2f}".format(valor_conta)
